af32af15b5450239df968867f2f60ebdef32f61f
Alexis Kofman <alexis.kofman@gmail.com>
2015-05-21 22:50:09 +0200

    init boilerplate

---
+++ src/ios/cordova-plugin-boilerplate.m
@@ -0,0 +1,28 @@
+/********* cordova-plugin-boilerplate.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface cordova-plugin-boilerplate : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation cordova-plugin-boilerplate
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
